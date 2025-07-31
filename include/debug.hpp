61f931cf166095bd8e22db1822a30cb420278a89
segcoredrakon <segcoredrakon@gmail.com>
2025-07-31 21:59:33 +0200

    add file information and remove comment

--- include/debug.hpp
+++ include/debug.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file debug.hpp
+ * @brief the header of the debug system of the program
+ * @author SegCoreDrakon
+ * @date 2025-03-31
+ * @version 0.0.3
+ */
+
 #pragma once
 #ifndef DEBUG_HPP
 #define DEBUG_HPP
@@ -6,9 +14,9 @@
 #include <source_location>
 
 namespace debugsys {
-    // void write_log(std::string data);
     void write_log(const std::string_view& message = "no message",
                    const std::source_location location =
                    std::source_location::current());
 }
+
 #endif // DEBUG_HPP
